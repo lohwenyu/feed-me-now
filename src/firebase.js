ab9d8ab8a7872edc93e7e0811abc080dfbd96467
lohwenyu <e0421622@u.nus.edu>
2021-03-18 18:46:07 +0800

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCS-IPg6leIzJAsNilhB9zjnYs17R4LMmg",
+    authDomain: "bt3103-feed-me-now.firebaseapp.com",
+    projectId: "bt3103-feed-me-now",
+    storageBucket: "bt3103-feed-me-now.appspot.com",
+    messagingSenderId: "1042117429489",
+    appId: "1:1042117429489:web:3f8acba267680f6e238150",
+    measurementId: "G-704VTEHLQH"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.firestore();
+
+export default database;
